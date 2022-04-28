0b604411c6eace2c58851c4b555da8a939857f5d
Dan Steinicke <dansteinicke@gmail.com>
2022-04-28 11:36:19 -0700

    Initial commit.  Demo onChange handler.

---
+++ app/routes/ThisWorksToo/$ThisWorksToo.jsx
@@ -0,0 +1,40 @@
+import { Form, useSubmit } from "@remix-run/react"
+
+export async function action({ request }) {
+  const data = await request.formData()
+  console.log("...data ", data)
+  return {}
+}
+
+const ThisWorksToo = () => {
+  const submit = useSubmit()
+
+  function handleChange(event) {
+    submit(event.currentTarget, { replace: true })
+  }
+
+  return (
+    <>
+      <h1>ThisWorksToo</h1>
+      <Form method="post" onChange={handleChange}>
+        <div>
+          <label htmlFor="approved">Approved</label>
+          <input type="checkbox" name="approved" value="approved" />
+        </div>
+        <div>
+          <select name="colorChoice" id="cc">
+            <option value="blue">blue</option>
+            <option value="yellow">yellow</option>
+            <option value="red">red</option>
+          </select>
+        </div>
+        <div>
+          <label>description</label>
+          <input type="text" name="description" id="discription" />
+        </div>
+      </Form>
+    </>
+  )
+}
+
+export default ThisWorksToo
---
+++ app/routes/index.jsx
@@ -0,0 +1,13 @@
+import { NavLink } from "react-router-dom"
+
+export default function Index() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <NavLink to="/ThisWorksToo/88">ThisWorksToo</NavLink>
+        </li>
+      </ul>
+    </nav>
+  )
+}
